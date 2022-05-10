ae33448d9bcd7502d9a7c530d365967fa3d50218
Robert <leifman.robert@gmail.com>
2022-05-10 18:58:06 +0300

    modal js

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let modalOverlay = document.querySelector('.modal__overlay')
+let buyButton = document.querySelector('.product-card__button');
+let modalWindow = document.querySelector('.modal__wrapper');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,3 +15,18 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+buyButton.addEventListener('click', function () {
+  if (modalOverlay.classList.contains('modal__overlay--closed')) {
+    modalOverlay.classList.remove('modal__overlay--closed');
+    modalOverlay.classList.add('modal__overlay--opened');
+    modalWindow.classList.remove('modal__wrapper--closed');
+    modalWindow.classList.add('modal__wrapper--opened');
+  } else {
+    modalOverlay.classList.add('modal__overlay--closed');
+    modalOverlay.classList.remove('modal__overlay--opened');
+    modalWindow.classList.add('modal__wrapper--closed');
+    modalWindow.classList.remove('modal__wrapper--opened');
+  }
+});
+
